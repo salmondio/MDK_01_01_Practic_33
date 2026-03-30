9398ea5fd88a9d44996904fc14c7c0b08580f7b1
asdas <adasdawdas>
2026-03-30 14:24:39 +0500

    разработанны модели сержанин

---
+++ Practic_33/Models/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practic_33.Models
+{
+    public class Users
+    {
+        /// <summary> Код </summary>
+        public int Id { get; set; }
+
+        /// <summary> Фамилия </summary>
+        public string Lastname { get; set; }
+
+        /// <summary> Имя пользователя </summary>
+        public string Firstname { get; set; }
+
+        /// <summary> Отчество </summary>
+        public string Surname { get; set; }
+
+        /// <summary> Фотография </summary>
+        public byte[] Photo { get; set; }
+
+        /// <summary> Конструктор для заполнения объекта </summary>
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+
+        /// <summary> Получить ФИО пользователя </summary>
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
