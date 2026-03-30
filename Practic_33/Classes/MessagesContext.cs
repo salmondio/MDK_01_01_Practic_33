4ad40756037a34b0f0fec82690b4f212ee55af2f
asdas <adasdawdas>
2026-03-30 14:34:13 +0500

    разработаны контексты данных Серажинн

---
+++ Practic_33/Classes/MessagesContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Practic_33.Classes.Common;
+using Practic_33.Models;
+
+namespace Practic_33.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        /// <summary> Данные из БД </summary>
+        public DbSet<Messages> Messages { get; set; }
+
+        /// <summary> Конструктор контекста </summary>
+        public MessagesContext() =>
+            Database.EnsureCreated();
+
+        /// <summary> Конфигурация подключения </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            // Говорим что используем SQL Server со следующей конфигурацией
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Practic_33/Classes/UsersContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Practic_33.Classes.Common;
+using Practic_33.Models;
+
+namespace Practic_33.Classes
+{
+    public class UsersContext : DbContext
+    {
+        /// <summary> Данные из БД </summary>
+        public DbSet<Users> Users { get; set; }
+
+        /// <summary> Конструктор контекста </summary>
+        public UsersContext() =>
+            // Проверяем подключены ли мы к БД, если не подключены, подключаемся
+            Database.EnsureCreated();
+
+        /// <summary> Конфигурация подключения </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            // Говорим что используем SQL Server со следующей конфигурацией
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
