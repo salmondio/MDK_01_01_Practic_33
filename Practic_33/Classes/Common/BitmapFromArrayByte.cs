ba17b807c509a16bc5605563aefed751922a2972
asdas <adasdawdas>
2026-03-30 14:26:55 +0500

    разработанны общие классы сержанин

---
+++ Practic_33/Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Practic_33.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        /// <summary> Создание битмапы изображения из массивов байт </summary>
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
