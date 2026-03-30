17e2f6197ab73dc2917d47fd8a0fa3357b13f48e
asdas <adasdawdas>
2026-03-30 14:52:12 +0500

    Разработанный пользовательский элемент Message. Фамилия - Serzhanin

---
+++ Practic_33/Pages/Items/Message.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practic_33.Classes.Common;
+using Practic_33.Models;
+
+namespace Practic_33.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, Users UserFrom)
+        {
+            InitializeComponent();
+
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
